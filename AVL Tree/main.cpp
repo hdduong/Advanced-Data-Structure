218766f96bf0e995d89136be908c7765b0885a86
hdduong <hdduong.at.wpi.edu@gmail.com>
2014-10-21 23:33:08 -0700

    re commit -1

--- AVL Tree/main.cpp
+++ AVL Tree/main.cpp
@@ -37,11 +37,28 @@ int sizeTree(node *root) {
 int sizeDifferent(node *root) {
 	return(sizeTree(root->left) - sizeTree(root->right));
 }
+
+
+int max(int a, int b) {
+	return (a > b)? a: b;
+}
+
+int heightTree(node *root) {
+	if (root == NULL) return 0;
+	return (1 + max(heightTree(root->left), heightTree(root->right) ));
+}
+
+int heightDifferent(node *root) {
+	if (root == NULL) return 0;
+	return (heightTree(root->left) - heightTree(root->right));
+}
+
+
 //balanced if size difference in [-1..1]
 // < -1 : right tree bigger than left tree
 // > 1 : left tree bigger than right tree
 bool isBalanced(node *root) { 
-	int difference = sizeDifferent(root);
+	int difference = heightDifferent(root);
 
 	if (abs(difference) <=1 ) return true;
 	return false;
@@ -91,9 +108,46 @@ node* doBalanceTree(node *z, node* y, node *x) {
 		y->right = z;
 		z->left = rightY;
 		z = y;
+
+		return z;
 	}
 
-	return z;
+	if ( (y == z->left) && (x == y->right) ) {
+		// left rotate (y)
+		node* leftX = x->left;
+		z->left = x;
+		x->left = y;
+		y->right = leftX;
+
+		// right rotate (z)
+		node* rightX = x->right;
+		x->left = z;
+		z->left = rightX;
+
+		return x;
+	}
+	
+	if ( (z->right == y) && (x == y->right) ) {
+		//left rotate z
+		node* leftY = y->left;
+		y->left = z;
+		z->right = leftY;
+
+		return y;
+	}
+
+	if ( (z->right == y) && (x == y->left) ) {
+		//right rotate y
+		y->left = x->right;
+		x->right = y; //leftZ
+		z->right = x;
+
+		//left rotate z
+		z->right = x->left;
+		x->left = z;
+
+		return x;
+	}
 }
 
 
@@ -106,8 +160,34 @@ void main() {
 	node* x = NULL;
 	bool foundPath = false;
 	root = insertNode(root,3, z, y, x, &foundPath);
+
+	foundPath = false;
 	root = insertNode(root,2, z, y, x, &foundPath);
+	
+	foundPath = false;
 	root = insertNode(root,1, z, y, x, &foundPath);
+	
+	foundPath = false;
+	root = insertNode(root,4, z, y, x, &foundPath);
+	
+	foundPath = false;
+	root = insertNode(root,5, z, y, x, &foundPath);
+
+	foundPath = false;
+	root = insertNode(root,6, z, y, x, &foundPath);
+
+	foundPath = false;
+	root = insertNode(root,7, z, y, x, &foundPath);
+
+	foundPath = false;
+	root = insertNode(root,16, z, y, x, &foundPath);
+
+	foundPath = false;
+	root = insertNode(root,15, z, y, x, &foundPath);
+
+	
+	foundPath = false;
+	root = insertNode(root,14, z, y, x, &foundPath);
 
 	cout<< "Tree inOrder: " ; 
 	printTreeInOrder(root);
